062a7a0c4252f35b680ee1c40696598606ec345e
faceman7353@gmail.com <faceman7353@gmail.com>
2022-04-07 18:05:21 +0900

    react-app

---
+++ src/components/Subject.js
@@ -0,0 +1,19 @@
+import { toHaveStyle } from '@testing-library/jest-dom/dist/matchers';
+import React, {Component} from 'react';
+
+class Subject extends Component{
+    render(){
+      console.log('Subject render');
+      return(
+        <header>
+          <h1><a href='/' onClick={function(e){
+            e.preventDefault();
+            this.props.onChagePage();
+          }.bind(this)}>{this.props.title}</a></h1>
+           {this.props.sub}       
+        </header>
+      )
+    }
+  }
+
+  export default Subject
